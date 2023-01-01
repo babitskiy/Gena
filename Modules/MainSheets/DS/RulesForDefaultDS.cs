f4480cc5a697d734ef0573c067de5db2b75cc793
Evgeniy Babiсkiy <babickii_@mail.ru>
2023-01-02 01:02:08 +0300

    1.0.0.0

--- Modules/MainSheets/DS/RulesForDefaultDS.cs
+++ Modules/MainSheets/DS/RulesForDefaultDS.cs
@@ -1,11 +1,6 @@
 ﻿using ClosedXML.Excel;
 using Gena.Templates.DS;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Gena.Modules.MainSheets.DS
 {
